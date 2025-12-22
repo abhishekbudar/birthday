cdfd6c4f5ac13b10884b6b67e0673f175dde43f3
abhi <abhishekbudar28@gmail.com>
2025-12-22 11:32:07 +0530

    initial

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS loaded successfully");
+const text = "Today is all about you 💖";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.querySelector(".hero p").textContent += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+document.querySelector(".hero p").textContent = "";
+typeEffect();
